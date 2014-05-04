9e594580d90ddfae0541a099c6062b4b72c3905c
Abram Hindle <abram.hindle@softwareprocess.es>
2014-05-03 19:34:38 -0600

    Better generator

--- generate.pl
+++ generate.pl
@@ -14,7 +14,8 @@
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governin
+#   See the License for the specific language governin permissions and
+#   limitations under the License.
 
 use strict;
 use JSON;
@@ -88,6 +89,21 @@ __DATA__
 <title><TMPL_VAR NAME="name"></title>
 <meta charset="UTF-8"/>
 </head>
+<!--
+#   Copyright 2014 Abram Hindle
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governin permissions and
+#   limitations under the License.
+-->
 <style>
 .box {
     padding: 0.5em;
